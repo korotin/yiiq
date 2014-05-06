1dd0df140f61f43eea53f4296281ebf36809ce4f
m4 <herr.offizier@gmail.com>
2014-05-06 20:20:46 +0400

    Added ability to watch several queues by single worker

--- commands/YiiqCommand.php
+++ commands/YiiqCommand.php
@@ -18,19 +18,24 @@ class YiiqCommand extends YiiqBaseCommand
     /**
      * Run worker for given queue.
      * 
-     * @param  string[optional] $queue  Yiiq::DEFAULT_QUEUE by default
+     * @param  array[optional] $queue  Yiiq::DEFAULT_QUEUE by default
      * @param  int[optional] $threads   Yiiq::DEFAULT_THREADS by default
      */
-    public function actionStart($queue = null, $threads = null)
+    public function actionStart(array $queue = null, $threads = null)
     {
         Yii::app()->getComponent('yiiq');
 
-        $queue = $queue ?: Yiiq::DEFAULT_QUEUE;
+        $queues = $queue ?: array(Yiiq::DEFAULT_QUEUE);
+        $stringifiedQueues = implode(', ', $queues);
         $threads = (int)$threads ?: Yiiq::DEFAULT_THREADS;
 
-        $command = 'nohup sh -c "'.escapeshellarg(Yii::app()->basePath.'/yiic').' yiiqWorker run --queue='.$queue.' --threads='.$threads.'" > /dev/null 2>&1 &';
+        $command = 'nohup sh -c "'.escapeshellarg(Yii::app()->basePath.'/yiic').' yiiqWorker run ';
+        foreach ($queues as $queue) {
+            $command .= '--queue='.$queue.' ';
+        }
+        $command .= '--threads='.$threads.'" > /dev/null 2>&1 &';
         $return = null;
-        echo "Starting worker for $queue ($threads threads)... ";
+        echo "Starting worker for $stringifiedQueues ($threads threads)... ";
         exec($command, $return);
         echo "Done.\n";
     }
--- commands/YiiqWorkerCommand.php
+++ commands/YiiqWorkerCommand.php
@@ -34,11 +34,17 @@ class YiiqWorkerCommand extends YiiqBaseCommand
      */
     protected $pid;
     /**
-     * Worker queue name.
+     * Worker queues.
+     * 
+     * @var array
+     */
+    protected $queues;
+    /**
+     * Stringifies queues names.
      * 
      * @var string
      */
-    protected $queue;
+    protected $stringifiedQueues;
     /**
      * Max count of child threads.
      * 
@@ -59,14 +65,21 @@ class YiiqWorkerCommand extends YiiqBaseCommand
      */
     protected $shutdown = false;
 
+    protected function stringifyQueues(array $queues)
+    {
+        asort($queues);
+        return implode(',', $queues);
+    }
+
     /**
      * Set process title to given string.
      * Process title is changing by cli_set_process_title (PHP >= 5.5) or
      * setproctitle (if proctitle extension is available).
      * 
      * @param string $title
+     * @param string[optional] $queue
      */
-    protected function setProcessTitle($title)
+    protected function setProcessTitle($title, $queue = null)
     {
         $titleTemplate = Yii::app()->yiiq->titleTemplate;
         if (!$titleTemplate) return;
@@ -74,7 +87,7 @@ class YiiqWorkerCommand extends YiiqBaseCommand
         $placeholders = array(
             '{name}'    => $this->instanceName,
             '{type}'    => $this->processType,
-            '{queue}'   => $this->queue,
+            '{queue}'   => $queue ?: $this->stringifiedQueues,
             '{message}' => $title,
         );
 
@@ -92,9 +105,9 @@ class YiiqWorkerCommand extends YiiqBaseCommand
      *  
      * @return string
      */
-    protected function getWorkerPidName()
+    protected function getWorkerPidName($queue)
     {
-        return Yii::app()->yiiq->prefix.':workers:'.$this->queue;
+        return Yii::app()->yiiq->prefix.':workers:'.$queue;
     }
 
     /**
@@ -109,6 +122,46 @@ class YiiqWorkerCommand extends YiiqBaseCommand
         }
 
         return $this->childPool;
+    }
+
+    /**
+     * Check if given queues are already watched.
+     */
+    protected function checkRunningWorkers()
+    {
+        foreach ($this->queues as $queue) {
+            if (
+                ($oldPid = Yii::app()->redis->get($this->getWorkerPidName($queue)))
+                && (Yii::app()->yiiq->isPidAlive($oldPid))
+            ) {
+                Yii::trace('Worker for queue '.$queue.' already running.');
+                exit(1);
+            } 
+        }
+    }
+
+    /**
+     * Save current pid to redis.
+     */
+    protected function savePid()
+    {
+        foreach ($this->queues as $queue) {
+            Yii::app()->redis->set($this->getWorkerPidName($queue), $this->pid);
+        }
+
+        Yii::app()->yiiq->pidPool->add($this->pid);
+    }
+
+    /**
+     * Remove current pid from redis.
+     */
+    protected function clearPid()
+    {
+        foreach ($this->queues as $queue) {
+            Yii::app()->redis->del($this->getWorkerPidName($queue));
+        }
+
+        Yii::app()->yiiq->pidPool->remove($this->pid);
     }
 
     /**
@@ -151,13 +204,23 @@ class YiiqWorkerCommand extends YiiqBaseCommand
     }
 
     /**
+     * Wait for child threads to complete.
+     */
+    protected function waitForThreads()
+    {
+        $status = null;
+        while (pcntl_waitpid(-1, $status) > 0) { }
+    }
+
+    /**
      * Run job with given id.
      * Job will be executed in fork and method will return 
      * after the fork get initialized.
-     * 
+     *
+     * @param  string $queue
      * @param  string $jobId
      */
-    protected function runJob($job)
+    protected function runJob($queue, $job)
     {
         // If we're in parent process, wait for child thread to get initialized
         // and then return
@@ -169,7 +232,7 @@ class YiiqWorkerCommand extends YiiqBaseCommand
         $childPid = posix_getpid();
         
         $this->processType = 'job';
-        $this->setProcessTitle('initializing');
+        $this->setProcessTitle('initializing', $queue);
 
         // Force reconnect to redis
         Yii::app()->redis->setClient(null);
@@ -177,12 +240,12 @@ class YiiqWorkerCommand extends YiiqBaseCommand
         $this->getChildPool()->add($childPid);
 
         extract($job);
-        Yii::trace('Starting job '.$this->queue.':'.$id.' ('.$class.')...');
-        $this->setProcessTitle('executing '.$id.' ('.$class.')');
+        Yii::trace('Starting job '.$queue.':'.$id.' ('.$class.')...');
+        $this->setProcessTitle('executing '.$id.' ('.$class.')', $queue);
 
         $e = null;
         try {
-            $job = new $class($this->queue, $type, $id);
+            $job = new $class($queue, $type, $id);
             $job->execute($args);
         }
         catch (Exception $e) {
@@ -192,7 +255,7 @@ class YiiqWorkerCommand extends YiiqBaseCommand
         $this->getChildPool()->remove($childPid);
 
         if (!$e) {
-            Yii::trace('Job '.$this->queue.':'.$id.' done.');
+            Yii::trace('Job '.$queue.':'.$id.' done.');
         }
         else {
             throw $e;
@@ -200,46 +263,37 @@ class YiiqWorkerCommand extends YiiqBaseCommand
 
         exit(0);
     }
-    
+
     /**
-     * Run worker for given queue and with given count of
-     * max child threads.
-     * 
-     * @param  string $queue 
-     * @param  int $threads
+     * Main loop.
      */
-    public function actionRun($queue, $threads)
+    protected function loop()
     {
-        $this->instanceName = Yii::app()->yiiq->name ?: Yii::getPathOfAlias('application');
-        $this->pid          = posix_getpid();
-        $this->queue        = $queue ?: Yiiq::DEFALUT_QUEUE;
-        $this->maxThreads   = (int) $threads;
-
-        $this->setProcessTitle('initializing');
-
-        if (
-            ($oldPid = Yii::app()->redis->get($this->getWorkerPidName()))
-            && (Yii::app()->yiiq->isPidAlive($oldPid))
-        ) {
-            Yii::trace('Worker for queue '.$this->queue.' already running.');
-            exit(1);
-        }
-
-        $this->setupSignals();
-
-        Yii::app()->redis->set($this->getWorkerPidName(), $this->pid);
-        Yii::app()->yiiq->pidPool->add($this->pid);
-
-        Yii::trace('Started new yiiq worker '.$this->pid.' for queue '.$this->queue.'.');
-
         $status = null;
+        $offset = null;
+        $count = count($this->queues);
+
         while (true) {
+            // Iterate over free threads.
             while (
                 !$this->shutdown
                 && $this->hasFreeThread()
             ) {
-                if (!($job = Yii::app()->yiiq->popJob($this->queue))) break;
-                $this->runJob($job);
+                // Look for new job.
+                // Iterate over all watched queues, stop when new job found.
+                for ($index = 0; $index < $count; $index++) {
+                    $queue = $this->queues[($index + $offset) % $count];
+                    if ($job = Yii::app()->yiiq->popJob($queue)) {
+                        $offset = ($index + 1) % $count;
+                        break;
+                    }
+                }
+                
+                // No job was found - exit loop.
+                if (!$job) break;
+
+                // Execute found job.
+                $this->runJob($queue, $job);
             }
 
             if ($this->shutdown) break;
@@ -247,33 +301,48 @@ class YiiqWorkerCommand extends YiiqBaseCommand
             // If all child threads are active, wait for one of them to terminate.
             // Otherwise just sleep for 1 second.
             if (!$this->hasFreeThread()) {
-                $this->setProcessTitle('no free threads ('.$threads.' threads)');
+                $this->setProcessTitle('no free threads ('.$this->maxThreads.' threads)');
                 pcntl_waitpid(-1, $status);
             }
             else {
-                $this->setProcessTitle('no new jobs ('.$this->getThreadsCount().' of '.$threads.' threads busy)');
+                $this->setProcessTitle('no new jobs ('.$this->getThreadsCount().' of '.$this->maxThreads.' threads busy)');
                 sleep(1);
             }
 
             // Check for dead pids in child pool
             Yii::app()->yiiq->checkPidPool($this->getChildPool());
         }
+    }
+    
+    /**
+     * Run worker for given queue and with given count of
+     * max child threads.
+     * 
+     * @param  array $queues 
+     * @param  int $threads
+     */
+    public function actionRun(array $queue, $threads)
+    {
+        $this->instanceName = Yii::app()->yiiq->name ?: Yii::getPathOfAlias('application');
+        $this->pid          = posix_getpid();
+        $this->queues       = $queue;
+        $this->stringifiedQueues = $this->stringifyQueues($this->queues);
+        $this->maxThreads   = (int) $threads;
 
-        Yii::trace('Waiting for child threads to terminate...');
-        $this->setProcessTitle('terminating ('.$this->getThreadsCount().' threads left)');
-        while (pcntl_waitpid(-1, $status) > 0) {
-            $threadsLeft = $this->getThreadsCount();
-            Yii::trace($threadsLeft.' threads left.');
-            if ($threadsLeft) {
-                $this->setProcessTitle('terminating ('.$threadsLeft.' threads left)');
-            }
-            else {
-                $this->setProcessTitle('terminating');
-            }
-        }
+        $this->setProcessTitle('initializing');
 
-        Yii::app()->redis->del($this->getWorkerPidName());
-        Yii::app()->yiiq->pidPool->remove($this->pid);
+        $this->checkRunningWorkers();
+        $this->setupSignals();
+        $this->savePid();
+
+        Yii::trace('Started new yiiq worker '.$this->pid.' for '.$this->stringifiedQueues.'.');
+
+        $this->loop();
+       
+        $this->setProcessTitle('terminating');
+        
+        $this->waitForThreads();
+        $this->clearPid();
         
         Yii::trace('Terminated yiiq worker '.$this->pid.'.');
     }
